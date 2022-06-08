30e6c2e846b7d375d1c0b2ca0159d2bddbf707a4
fabian-ss <randommusicd@gmail.com>
2022-06-07 20:03:11 -0500

    first commit

---
+++ server.py
@@ -0,0 +1,17 @@
+import socket
+import asyncio
+from hypercorn.asyncio import serve
+from hypercorn.config import Config
+from main import app
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+host = s.getsockname()[0]
+s.close()
+
+config = Config()
+config.bind = [host+":12345"] 
+
+asyncio.run(serve(app, config))
+
+#https://pgjones.gitlab.io/hypercorn/how_to_guides/api_usage.html
